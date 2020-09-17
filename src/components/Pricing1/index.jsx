0195a06364434f7ea16d4995a92921946412b68b
Josh Wang <josh.wang@lmigroup.com>
2020-09-17 23:17:49 +1000

    feat: add 3 Pricing components

--- src/components/Pricing1/index.jsx
+++ src/components/Pricing1/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
-import QueueAnim from 'rc-queue-anim';
-import { Row, Col, Button } from 'antd';
+import React from "react";
+import OverPack from "rc-scroll-anim/lib/ScrollOverPack";
+import QueueAnim from "rc-queue-anim";
+import { Row, Col, Button } from "antd";
 /* replace-start-value = import { getChildrenToRender } from './utils'; */
-import { getChildrenToRender } from '../../utils';
+import { getChildrenToRender } from "../../utils";
 /* replace-end-value */
 /* replace-start */
-import './index.less';
+import "./index.less";
 /* replace-end */
+import dataSource from "./data.source";
 
 class Pricing1 extends React.PureComponent {
   getChildrenToRender = (item) => {
@@ -33,7 +34,7 @@ class Pricing1 extends React.PureComponent {
             <div {...name} key="name">
               {
                 /* replace-start-value = name.children */
-                React.createElement('span', {
+                React.createElement("span", {
                   dangerouslySetInnerHTML: { __html: name.children },
                 })
                 /* replace-end-value */
@@ -42,7 +43,7 @@ class Pricing1 extends React.PureComponent {
             <h1 {...money} key="money">
               {
                 /* replace-start-value = money.children */
-                React.createElement('span', {
+                React.createElement("span", {
                   dangerouslySetInnerHTML: { __html: money.children },
                 })
                 /* replace-end-value */
@@ -52,7 +53,7 @@ class Pricing1 extends React.PureComponent {
           <div {...content} key="content">
             {
               /* replace-start-value = content.children */
-              React.createElement('span', {
+              React.createElement("span", {
                 dangerouslySetInnerHTML: { __html: content.children },
               })
               /* replace-end-value */
@@ -68,8 +69,10 @@ class Pricing1 extends React.PureComponent {
             >
               {
                 /* replace-start-value = buttonWrapper.children.a.children */
-                React.createElement('span', {
-                  dangerouslySetInnerHTML: { __html: buttonWrapper.children.a.children },
+                React.createElement("span", {
+                  dangerouslySetInnerHTML: {
+                    __html: buttonWrapper.children.a.children,
+                  },
                 })
                 /* replace-end-value */
               }
@@ -104,7 +107,7 @@ class Pricing1 extends React.PureComponent {
               type="bottom"
               component={Row}
               leaveReverse
-              ease={['easeOutQuad', 'easeInOutQuad']}
+              ease={["easeOutQuad", "easeInOutQuad"]}
               key="content"
               /* replace-start */
               data-edit="Row"
@@ -119,4 +122,4 @@ class Pricing1 extends React.PureComponent {
   }
 }
 
-export default Pricing1;
+export default { component: Pricing1, dataSource };
