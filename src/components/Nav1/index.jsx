384bde7c4ea29206b2cc3a0bd08e025f9b264501
Josh Wang <josh.wang@lmigroup.com>
2020-09-17 23:14:09 +1000

    feat: add 3 Nav components

--- src/components/Nav1/index.jsx
+++ src/components/Nav1/index.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { findDOMNode } from 'react-dom';
-import TweenOne from 'rc-tween-one';
-import { Menu } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import React from "react";
+import { findDOMNode } from "react-dom";
+import TweenOne from "rc-tween-one";
+import { Menu } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 /* replace-start */
-import { polyfill } from 'react-lifecycles-compat';
-import './index.less';
+import { polyfill } from "react-lifecycles-compat";
+import "./index.less";
 /* replace-end */
+import dataSource from "./data.source";
 
 const { Item, SubMenu } = Menu;
 
@@ -39,44 +40,38 @@ class Header extends React.Component {
       phoneOpen,
       menuHeight: phoneOpen ? menu.scrollHeight : 0,
     });
-  }
+  };
 
   render() {
     const { dataSource, isMobile, ...props } = this.props;
     const { menuHeight, phoneOpen } = this.state;
     const navData = dataSource.Menu.children;
-    const navChildren = Object.keys(navData)
-      .map((key, i) => {
-        const { a, ...navProps } = navData[key];
-        return (
-          <Item
-            {...navProps}
-            key={i.toString()}
-            /* replace-start */
-            data-edit="Menu"
+    const navChildren = Object.keys(navData).map((key, i) => {
+      const { a, ...navProps } = navData[key];
+      return (
+        <Item
+          {...navProps}
+          key={i.toString()}
+          /* replace-start */
+          data-edit="Menu"
           /* replace-end */
-          >
-            <a
-              {...a}
-              href={a.link}
-              target={a.blank && '_blank'}
-            >
-              {
-                /* replace-start-value = a.children */
-                React.createElement('span', { dangerouslySetInnerHTML: { __html: a.children } })
-                /* replace-end-value */
-              }
-            </a>
-          </Item>
-        );
-      }
+        >
+          <a {...a} href={a.link} target={a.blank && "_blank"}>
+            {
+              /* replace-start-value = a.children */
+              React.createElement("span", {
+                dangerouslySetInnerHTML: { __html: a.children },
+              })
+              /* replace-end-value */
+            }
+          </a>
+        </Item>
       );
+    });
 
     // user 涉及到数据，请自行替换。
     const userTitle = (
-      <div
-        {...dataSource.user}
-      >
+      <div {...dataSource.user}>
         <span className="img" {...dataSource.user.img}>
           <img
             src="https://zos.alipayobjects.com/rmsportal/iXsgowFDTJtGpZM.png"
@@ -85,62 +80,53 @@ class Header extends React.Component {
             alt="img"
           />
         </span>
-        <span>
-          用户名
-        </span>
+        <span>用户名</span>
       </div>
     );
     navChildren.push(
-      (
-        <Item {...dataSource.help} key="help">
-          <QuestionCircleOutlined />
-          <span>
-            {
-              /* replace-start-value = dataSource.help.children */
-              React.createElement('span', { dangerouslySetInnerHTML: { __html: dataSource.help.children } })
-              /* replace-end-value */
-            }
-          </span>
-        </Item>),
-      (
-        <SubMenu
-          className="user"
-          title={userTitle}
-          key="user"
-        >
-          <Item key="a">
-            用户中心
-          </Item>
-          <Item key="b">
-            修改密码
-          </Item>
-          <Item key="c">
-            登出
-          </Item>
-        </SubMenu>)
+      <Item {...dataSource.help} key="help">
+        <QuestionCircleOutlined />
+        <span>
+          {
+            /* replace-start-value = dataSource.help.children */
+            React.createElement("span", {
+              dangerouslySetInnerHTML: { __html: dataSource.help.children },
+            })
+            /* replace-end-value */
+          }
+        </span>
+      </Item>,
+      <SubMenu className="user" title={userTitle} key="user">
+        <Item key="a">用户中心</Item>
+        <Item key="b">修改密码</Item>
+        <Item key="c">登出</Item>
+      </SubMenu>
     );
     const menuProps = {
-      mode: isMobile ? 'inline' : 'horizontal',
-      defaultSelectedKeys: ['0'],
-      theme: isMobile ? 'dark' : 'default',
+      mode: isMobile ? "inline" : "horizontal",
+      defaultSelectedKeys: ["0"],
+      theme: isMobile ? "dark" : "default",
     };
     if (isMobile) {
-      menuProps.openKeys = ['user'];
+      menuProps.openKeys = ["user"];
     }
     return (
       <TweenOne
         component="header"
-        animation={{ opacity: 0, type: 'from' }}
+        animation={{ opacity: 0, type: "from" }}
         {...dataSource.wrapper}
         {...props}
       >
         <div
           {...dataSource.page}
-          className={`${dataSource.page.className}${phoneOpen ? ' open' : ''}`}
+          className={`${dataSource.page.className}${phoneOpen ? " open" : ""}`}
         >
           <TweenOne
             animation={{
-              x: -30, delay: 100, type: 'from', ease: 'easeOutQuad',
+              x: -30,
+              delay: 100,
+              type: "from",
+              ease: "easeOutQuad",
             }}
             {...dataSource.logo}
           >
@@ -154,7 +140,7 @@ class Header extends React.Component {
               }}
               /* replace-start */
               data-edit="Menu"
-            /* replace-end */
+              /* replace-end */
             >
               <em />
               <em />
@@ -163,18 +149,16 @@ class Header extends React.Component {
           )}
           <TweenOne
             {...dataSource.Menu}
-            animation={{ x: 30, type: 'from', ease: 'easeOutQuad' }}
-            ref={(c) => { this.menu = c; }}
+            animation={{ x: 30, type: "from", ease: "easeOutQuad" }}
+            ref={(c) => {
+              this.menu = c;
+            }}
             style={isMobile ? { height: menuHeight } : null}
             /* replace-start */
             data-edit="Menu"
-          /* replace-end */
+            /* replace-end */
           >
-            <Menu
-              {...menuProps}
-            >
-              {navChildren}
-            </Menu>
+            <Menu {...menuProps}>{navChildren}</Menu>
           </TweenOne>
         </div>
       </TweenOne>
@@ -183,5 +167,5 @@ class Header extends React.Component {
 }
 
 /* replace-start-value = export default Header */
-export default polyfill(Header);
+export default { component: polyfill(Header), dataSource };
 /* replace-end-value */
