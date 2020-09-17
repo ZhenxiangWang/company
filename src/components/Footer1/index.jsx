de9b6e069b32317c2b92895bf8be2d40e8f1cfdc
Josh Wang <josh.wang@lmigroup.com>
2020-09-17 23:10:35 +1000

    feat: add 3 Footer components

--- src/components/Footer1/index.jsx
+++ src/components/Footer1/index.jsx
@@ -1,56 +1,62 @@
-import React from 'react';
-import TweenOne from 'rc-tween-one';
-import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
-import QueueAnim from 'rc-queue-anim';
-import { Row, Col } from 'antd';
+import React from "react";
+import TweenOne from "rc-tween-one";
+import OverPack from "rc-scroll-anim/lib/ScrollOverPack";
+import QueueAnim from "rc-queue-anim";
+import { Row, Col } from "antd";
 /* replace-start-value = import { getChildrenToRender } from './utils'; */
-import { getChildrenToRender } from '../../utils';
+import { getChildrenToRender } from "../../utils";
 /* replace-end-value */
 /* replace-start-value = import { isImg } from './utils'; */
-import { isImg } from '../../../../utils';
+import { isImg } from "../../../../utils";
 /* replace-end-value */
 /* replace-start */
-import './index.less';
+import "./index.less";
 /* replace-end */
+import dataSource from "./data.source";
 
 class Footer extends React.Component {
   static defaultProps = {
-    className: 'footer1',
+    className: "footer1",
   };
 
-  getLiChildren = (data) => data.map((item, i) => {
-    const { title, childWrapper, ...itemProps } = item;
-    return (
-      <Col
-        key={i.toString()}
-        {...itemProps}
-        title={null}
-        content={null}
-        /* replace-start */
-        data-edit="Col"
-      /* replace-end */
-      >
-        <h2
-          {...title}
+  getLiChildren = (data) =>
+    data.map((item, i) => {
+      const { title, childWrapper, ...itemProps } = item;
+      return (
+        <Col
+          key={i.toString()}
+          {...itemProps}
+          title={null}
+          content={null}
           /* replace-start */
-          data-edit="image,text"
+          data-edit="Col"
           /* replace-end */
         >
-          {
-            typeof title.children === 'string' && title.children.match(isImg) ? (
-              <img src={title.children} width="100%" alt="img" />
-            ) : /* replace-start-value = title.children */React.createElement('span', { dangerouslySetInnerHTML: { __html: title.children } })
-            /* replace-end-value */
-          }
-        </h2>
-        <div {...childWrapper}>
-          {
-            childWrapper.children.map(getChildrenToRender)
-          }
-        </div>
-      </Col>
-    );
-  });
+          <h2
+            {...title}
+            /* replace-start */
+            data-edit="image,text"
+            /* replace-end */
+          >
+            {
+              typeof title.children === "string" &&
+              title.children.match(isImg) ? (
+                <img src={title.children} width="100%" alt="img" />
+              ) : (
+                /* replace-start-value = title.children */ React.createElement(
+                  "span",
+                  { dangerouslySetInnerHTML: { __html: title.children } }
+                )
+              )
+              /* replace-end-value */
+            }
+          </h2>
+          <div {...childWrapper}>
+            {childWrapper.children.map(getChildrenToRender)}
+          </div>
+        </Col>
+      );
+    });
 
   render() {
     const { ...props } = this.props;
@@ -60,9 +66,7 @@ class Footer extends React.Component {
     const childrenToRender = this.getLiChildren(dataSource.block.children);
     return (
       <div {...props} {...dataSource.wrapper}>
-        <OverPack
-          {...dataSource.OverPack}
-        >
+        <OverPack {...dataSource.OverPack}>
           <QueueAnim
             type="bottom"
             key="ul"
@@ -71,12 +75,12 @@ class Footer extends React.Component {
             {...dataSource.block}
             /* replace-start */
             data-edit="Row"
-          /* replace-end */
+            /* replace-end */
           >
             {childrenToRender}
           </QueueAnim>
           <TweenOne
-            animation={{ y: '+=30', opacity: 0, type: 'from' }}
+            animation={{ y: "+=30", opacity: 0, type: "from" }}
             key="copyright"
             {...dataSource.copyrightWrapper}
           >
@@ -84,7 +88,11 @@ class Footer extends React.Component {
               <div {...dataSource.copyright}>
                 {
                   /* replace-start-value = dataSource.copyright.children */
-                  React.createElement('span', { dangerouslySetInnerHTML: { __html: dataSource.copyright.children } })
+                  React.createElement("span", {
+                    dangerouslySetInnerHTML: {
+                      __html: dataSource.copyright.children,
+                    },
+                  })
                   /* replace-end-value */
                 }
               </div>
@@ -96,4 +104,4 @@ class Footer extends React.Component {
   }
 }
 
-export default Footer;
+export default { component: Footer, dataSource };
