d885a645f9b3f6b699631222adea6dc2945572cd
Josh Wang <josh.wang@lmigroup.com>
2020-09-18 21:57:48 +1000

    feat: add a template page

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -6,12 +6,8 @@ import { enquireScreen } from "enquire-js";
 import Layout0 from "../../components/layout0";
 import Banner0 from "../../components/banner0";
 import Content0 from "../../components/content0";
+import Content5 from "../../components/content5";
 import Content3 from "../../components/content3";
-import Feature6 from "../../components/feature6";
-import Feature8 from "../../components/feature8";
-import Pricing2 from "../../components/pricing2";
-import Content9 from "../../components/content9";
-import Teams3 from "../../components/teams3";
 
 import "../../static/index.less";
 
@@ -49,40 +45,16 @@ export default class Home extends React.Component {
         dataSource={Content0.dataSource}
         isMobile={this.state.isMobile}
       />,
+      <Content5.component
+        id="Content5_0"
+        key="Content5_0"
+        dataSource={Content5.dataSource}
+        isMobile={this.state.isMobile}
+      />,
       <Content3.component
         id="Content3_0"
         key="Content3_0"
         dataSource={Content3.dataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Feature6.component
-        id="Feature6_0"
-        key="Feature6_0"
-        dataSource={Feature6.dataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Feature8.component
-        id="Feature8_0"
-        key="Feature8_0"
-        dataSource={Feature8.dataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Pricing2.component
-        id="Pricing2_0"
-        key="Pricing2_0"
-        dataSource={Pricing2.dataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content9.component
-        id="Content9_0"
-        key="Content9_0"
-        dataSource={Content9.dataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Teams3.component
-        id="Teams3_0"
-        key="Teams3_0"
-        dataSource={Teams3.dataSource}
         isMobile={this.state.isMobile}
       />,
     ];
